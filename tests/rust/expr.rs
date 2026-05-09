f8d7860c275c8d748db1d509b9205abd1269058c
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-09 18:17:28 +0200

    fix expression test

--- tests/rust/expr.rs
+++ tests/rust/expr.rs
@@ -1,9 +1,8 @@
 // TODO: Should evaluate to 42, but compiler generates code that evaluates to 48
-fn main() {
-    let x = (**&&((1241 as u8 == (1241 % 256) as u8) as usize
-        + (13 - (27 > 128) as usize) as u8 as usize)
+fn main() -> usize {
+    (**&&((1241 as u8 == (1241 % 256) as u8) as usize + (13 - (27 > 128) as usize) as u8 as usize)
         - (10 as u8 / ((*&0 < 2) as usize + (4 != 3) as usize) as u8) as usize % 3)
         * (4 + (3 <= 3) as usize - (3 <= 4) as usize)
         - (4 as u8 - (5 >= 5) as u8 + (9 >= 0) as u8) as usize
-        - 2;
+        - 2
 }
