8a64985628dfa7f65190a4d8364324ce531da7ca
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-09 19:00:28 +0200

    test for associativity of term

--- tests/rust/expr.rs
+++ tests/rust/expr.rs
@@ -1,7 +1,6 @@
-// TODO: Should evaluate to 42, but compiler generates code that evaluates to 48
 fn main() -> usize {
     (**&&((1241 as u8 == (1241 % 256) as u8) as usize + (13 - (27 > 128) as usize) as u8 as usize)
-        - (10 as u8 / ((*&0 < 2) as usize + (4 != 3) as usize) as u8) as usize % 3)
+        - ((10 / 2 * 2) as u8 / ((*&0 < 2) as usize + (4 != 3) as usize) as u8) as usize % 3)
         * (4 + (3 <= 3) as usize - (3 <= 4) as usize)
         - (4 as u8 - (5 >= 5) as u8 + (9 >= 0) as u8) as usize
         - 2
