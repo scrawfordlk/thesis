4c9fe35775aaa649cefc39eb3c913d9a0a509a29
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-15 17:06:51 +0200

    test nested blocks

--- tests/rust/expr.rs
+++ tests/rust/expr.rs
@@ -1,8 +1,8 @@
-fn main() -> usize {
+fn main() {
     (**&&(({ 1241 as u8 } == (1241 % 256) as u8) as usize
         + (13 - (27 > 128) as usize) as u8 as usize)
         - ((10 / 2 * 2) as u8 / ((*&0 < 2) as usize + (4 != 3) as usize) as u8) as usize % 3)
-        * ({ 4 + (3 <= 3) as usize } - (3 <= 4) as usize)
-        - (4 as u8 - ({ 5 >= 5 }) as u8 + ({ 9 } >= 0) as u8) as usize
-        - { 2 }
+        * ({ 4 + { (3 <= 3) } as usize } - (3 <= 4) as usize)
+        - { 4 as u8 - ({ 5 >= { 5 } }) as u8 + ({ 9 } >= 0) as u8 } as usize
+        - { 2 };
 }
--- tests/rust/immutable_let.rs
+++ tests/rust/immutable_let.rs
@@ -2,7 +2,11 @@ fn main() -> usize {
     let a: usize = 2;
     let b: usize = {
         let a: u8 = 5 as u8;
-        let b: u8 = 16 as u8;
+        let b: u8 = {
+            let a: usize = 6;
+            let b: usize = 10;
+            a + b
+        } as u8;
         (a + b) as usize
     };
     let c: usize = a * b;
