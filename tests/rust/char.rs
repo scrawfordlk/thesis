979bc982b5d0aaaf32f0fa57b881abd4a9be1351
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-16 17:26:17 +0200

    fix erronous character test
    The test returned unit, which makes it always pass (even if it the value
    was incorrect).

--- tests/rust/char.rs
+++ tests/rust/char.rs
@@ -1,7 +1,7 @@
-fn main() {
+fn main() -> usize {
     ('\\' as u8
     - ((48 as u8 as char) < '1') as u8 // since integer literals are of usize type, cast into u8 before char 
     - ('\n' as usize + 'A' as usize) as u8
         + ('z' == 'z') as u8
-        + 25 as u8) as usize;
+        + 25 as u8) as usize
 }
